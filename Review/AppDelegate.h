1eb93aa2cff2a1f921342b58ffa290596667850e
Ben Norris <ben@bsn.io>
2015-08-04 18:18:07 -0600

    Initial Commit

---
+++ Review/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Review
+//
+//  Created by Ben Norris on 8/4/15.
+//  Copyright (c) 2015 Ben Norris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Review/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Review
+//
+//  Created by Ben Norris on 8/4/15.
+//  Copyright (c) 2015 Ben Norris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
